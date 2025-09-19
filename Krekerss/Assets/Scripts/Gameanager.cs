25119055c56bfb8f73f2d11c0ab187fca463e01a
TeraStale <terastale@gmail.com>
2025-09-19 04:44:42 +0300

    Krekers 0.01
    Shop logic 50%

---
+++ Krekerss/Assets/Scripts/Gameanager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3bd2a312d0f7df804c8c595eefa28584e0af62195920c4beee566fa0bf2d0e0d
+size 1306
---
+++ Krekerss/Assets/Scripts/StoreUpgrade.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b6361305d01748d24107b22c6f6a6404b2a4d0f72688539544fac10a37d9a8c
+size 1180
